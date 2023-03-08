7688a4222a3b19350c4a622a89200683437daba9
raghu0056 <raghumani0056@gmail.com>
2023-03-08 07:54:34 +0530

    flip coin and print percentage of head and tails

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int loopCount = 0;
+        double result;
+        double heads = 0;
+        if (Math.random() < 0.5){
+            System.out.println("Heads");
+        }else{
+            System.out.println("Tails");
+        }
+        while(loopCount <= 100)
+        {
+            result = Math.random();
+            if(result <= 0.5)
+                heads++;
+            loopCount ++;
+
+        }
+        double percentHeads = heads/100*100;
+        double percentTails = (100-heads)/100*100;
+        System.out.println("Percentage of heads is: " +percentHeads);
+        System.out.println("Percentage of tails is: " +percentTails);
     }
 }
