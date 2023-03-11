c7bb46fb989e501db55f61fb5b80449eb181bbb7
raghu0056 <raghumani0056@gmail.com>
2023-03-11 17:55:12 +0530

    Program for swap two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
-public class Main {
-
+import java.util.Scanner;
+class Main {
     public static void main(String[] args) {
-
-        int dividend = 25;
-        int divisor = 4;
-
-        int quotient = dividend / divisor;
-        int remainder = dividend % divisor;
-
-        System.out.println("Quotient = " + quotient);
-        System.out.println("Remainder = " + remainder);
+        int x;
+        int y;
+        int t;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of X and Y");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("before swapping numbers: "+x +"  "+ y);
+        t = x;
+        x = y;
+        y = t;
+        System.out.println("After swapping: "+x +"   " + y);
+        System.out.println( );
     }
-}
+}  
