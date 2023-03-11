578becc599281a69b9a195871c658c7f0827c403
raghu0056 <raghumani0056@gmail.com>
2023-03-11 17:58:37 +0530

    program for odd or even

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
-class Main {
+
+public class Main {
+
     public static void main(String[] args) {
-        int x;
-        int y;
-        int t;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of X and Y");
-        x = sc.nextInt();
-        y = sc.nextInt();
-        System.out.println("before swapping numbers: "+x +"  "+ y);
-        t = x;
-        x = y;
-        y = t;
-        System.out.println("After swapping: "+x +"   " + y);
-        System.out.println( );
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
     }
-}  
+}
